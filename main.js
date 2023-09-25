410a7a90c1c30b0685781a92dfa2131a11f541d8
molhodecarne3 <114995640+molhodecarne3@users.noreply.github.com>
2023-09-25 12:36:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function pom(){
+document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = pom;
+
+function clap(){
+    document.querySelector('#som_tecla_clap').play();
+    }
+    document.querySelector('.tecla_clap').onclick = clap;
+
+    function tim(){
+        document.querySelector('#som_tecla_tim').play();
+        }
+        document.querySelector('.tecla_tim').onclick = tim;
+
+        function puff(){
+            document.querySelector('#som_tecla_puff').play();
+            }
+            document.querySelector('.tecla_puff').onclick = puff;
+    
